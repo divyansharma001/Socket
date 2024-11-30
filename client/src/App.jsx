7a4fe3b3b402b65b92f6b4b7d2ec900c95589327
Divyansh Sharma <divyanshsharma4507@gmail.com>
2024-11-30 18:19:23 +0530

    feat: implement real-time messaging with socket.io and enhance UI with Material-UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,49 @@
-import React from 'react'
-import {io} from 'socket.io-client'
+import React, { useEffect, useMemo, useState } from "react";
+import { Button, Container, TextField, Typography } from "@mui/material";
+import { io } from "socket.io-client";
 
 const App = () => {
-  const socket = io(import.meta.env.VITE_BACKEND_URL)
+  const socket = useMemo(() => io(import.meta.env.VITE_BACKEND_URL), []);
+  // const socket = io(import.meta.env.VITE_BACKEND_URL);
+  const [message, setMessage] = useState("");
+ 
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log("Connected to server", socket.id);
+    });
+    socket.on("message", (data) => {
+      console.log(data);
+    });
 
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    socket.emit("message", message);
+    setMessage("");
+  };
   return (
-    <div>App</div>
-  )
-}
-export default App
+    <Container maxWidth="sm">
+      <Typography variant="h1" component="div" gutterBottom>
+        Welcome to Socket.io
+      </Typography>
+
+      <form onSubmit={handleSubmit}>
+        <TextField
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          id="outlined-basic"
+          label="outlined"
+          variant="outlined"
+        />
+        <Button type="submit" variant="contained" color="primary">
+          Send
+        </Button>
+      </form>
+    </Container>
+  );
+};
+export default App;
--- server/server.js
+++ server/server.js
@@ -1,17 +1,33 @@
 import express from 'express';
 import {Server} from 'socket.io';
 import {createServer} from 'http';
+import cors from 'cors'
+import 'dotenv/config' 
 const PORT = 3000;
 
 const app = express();
 
+app.use(cors())
+
 const server = createServer(app);
 
-const io = new Server
+const io = new Server(server, {
+  cors:{
+    origin: process.env.FRONTEND_URL,
+    methods: ['GET', 'POST'],
+    credentials: true
+  }
+})
 
 io.on('connection', (socket)=>{
-console.log('a user connected');
-console.log("id", socket.id);
+console.log('a user connected', socket.id);
+socket.on('message', (data)=>{
+  console.log(data);
+});
+socket.on('disconnect', ()=>{
+  console.log('user disconnected');
+  socket.broadcast.emit('message', `${socket.id} has left the chat`);
+});
 })
 
 
@@ -19,6 +35,6 @@ app.get('/', (req, res) => {
 res.send('Backend is up and running');
 })
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
