b0069e5e00903ddba97adb7077b454ed62a5a0fb
Divyansh Sharma <divyanshsharma4507@gmail.com>
2024-11-30 16:09:38 +0530

    refactor: update client structure and integrate socket.io for real-time communication

--- server/server.js
+++ server/server.js
@@ -1,10 +1,22 @@
 import express from 'express';
+import {Server} from 'socket.io';
+import {createServer} from 'http';
 const PORT = 3000;
 
 const app = express();
 
+const server = createServer(app);
+
+const io = new Server
+
+io.on('connection', (socket)=>{
+console.log('a user connected');
+console.log("id", socket.id);
+})
+
+
 app.get('/', (req, res) => {
-res.send('Hello World');
+res.send('Backend is up and running');
 })
 
 app.listen(PORT, () => {
